4208d46c1ee2edda6279167fe6d2a0638188c335
Ricardo Kovalski <ricardokovalskicruz@gmail.com>
2019-10-23 13:05:12 -0300

    Create Type Dead.php

---
+++ src/Types/Dead.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Holidays\Types;
+
+use Holidays\AbstractHoliday
+
+class Dead extends AbstractHoliday
+{
+    protected function name()
+    {
+        return "Finados";
+    }
+    
+    protected function date()
+    {
+        return date($this->formatter(), $this->timestamp());
+    }
+    
+    protected function national()
+    {
+        return true;
+    }
+    
+    public function timestamp()
+    {
+        return strtotime($this->getYear() . "-11-02");
+    }
+}

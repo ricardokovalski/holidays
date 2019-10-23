73db3f340a3fc37588b6e58998b8f7066a6c5b7c
Ricardo Kovalski <ricardokovalskicruz@gmail.com>
2019-10-23 13:03:47 -0300

    Create Type ProclamationRepublic.

---
+++ src/Types/ProclamationRepublic.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Holidays\Types;
+
+use Holidays\AbstractHoliday;
+
+class ProclamationRepublic extends AbstractHoliday
+{
+    protected function name()
+    {
+        return "Proclamação da República";
+    }
+    
+    protected function date()
+    {
+        return date($this->formatter(), $this->timestamp());
+    }
+    
+    protected function national()
+    {
+        return true;
+    }
+    
+    public function timestamp()
+    {
+        return strtotime($this->getYear() . "-11-15");
+    }
+}

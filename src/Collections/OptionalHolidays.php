d703c03864098117afe46473730b01c439df8fdd
Ricardo Kovalski <ricardokovalskicruz@gmail.com>
2020-02-24 17:36:17 +0000

    Criando a classe de coleção OptionalHolidays

---
+++ src/Collections/OptionalHolidays.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Holidays\Collections;
+
+use Holidays\Contract\Holiday;
+
+class OptionalHolidays extends AllHolidays
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->make();
+    }
+
+    private function make()
+    {
+        $holidays = array_filter($this->getCollection(), function (Holiday $holiday) {
+            return $holiday->getType() == \Holidays\Domain\TypeHoliday::OPTIONAL_HOLIDAY;
+        });
+
+        $this->collection = array_values($holidays);
+    }
+}

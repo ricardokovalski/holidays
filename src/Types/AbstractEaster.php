b63e35cb38e7295219477422c538eae5c9b8823a
Ricardo Kovalski <ricardokovalskicruz@gmail.com>
2020-06-19 13:43:09 -0300

    Criando uma classe abstrata contendo métodos específicos para datas relacionadas a Páscoa.

---
+++ src/Types/AbstractEaster.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Holidays\Types;
+
+use Holidays\Contract\Holiday;
+
+/**
+ * Class AbstractEaster
+ * @package Holidays\Types
+ */
+abstract class AbstractEaster extends AbstractHoliday
+{
+    /**
+     * @return float|int
+     */
+    protected function getNumberSecondsFromOneDay()
+    {
+        return 60 * 60 * 24;
+    }
+
+    /**
+     * @param $year
+     * @return int
+     */
+    protected function getDateEaster($year)
+    {
+        if ($year) {
+            return easter_date($year);
+        }
+
+        return easter_date();
+    }
+
+}

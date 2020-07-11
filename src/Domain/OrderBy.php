e9d0234f02596b1c4aa00c0498fda683d274a6c8
Ricardo Kovalski <ricardokovalskicruz@gmail.com>
2020-07-11 15:58:32 -0300

    Criando a classe de dindin OrderBy.

---
+++ src/Domain/OrderBy.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Holidays\Domain;
+
+/**
+ * Class OrderBy
+ * @package Holidays\Domain
+ */
+class OrderBy extends EnumType
+{
+    const GET_NAME = 'getName';
+
+    const GET_TIMESTAMP = 'getTimestamp';
+}

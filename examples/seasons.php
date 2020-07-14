95504aaf51a87748d9960f4ac2b458b9eb506e8f
Ricardo Kovalski <ricardokovalskicruz@gmail.com>
2020-07-14 08:39:25 -0300

    Criando novos exemplos.

---
+++ examples/seasons.php
@@ -0,0 +1,13 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+require __DIR__ . '../vendor/autoload.php';
+
+use Holidays\Collections\Seasons;
+
+$collection = new Seasons();
+$collection->orderByTimestamp()
+    ->ascending()
+    ->getCollection();

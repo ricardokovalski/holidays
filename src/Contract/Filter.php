9b57700a66c4ab7816c3735204ad2db374c1453d
Ricardo Kovalski <ricardokovalskicruz@gmail.com>
2020-07-14 08:40:51 -0300

    Implementando o contrato Filter.

---
+++ src/Contract/Filter.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Holidays\Contract;
+
+/**
+ * Interface Filter
+ * @package Holidays\Contract
+ */
+interface Filter
+{
+    /**
+     * @return mixed
+     */
+    public function execute();
+}

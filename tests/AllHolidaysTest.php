e0a1e8b24ee86de86f9531b517747089185b3392
Ricardo Kovalski <ricardokovalskicruz@gmail.com>
2020-08-13 15:34:35 -0300

    Criando alguns testes para os métodos de filtro.

--- tests/AllHolidaysTest.php
+++ tests/AllHolidaysTest.php
@@ -1,20 +1,14 @@
 <?php
 
-class HolidaysTest
+class AllHolidaysTest extends \PHPUnit_Framework_TestCase
 {
     private $collection;
+    private $actualYear;
 
     public function setUp()
     {
         $this->collection = new Holidays\Collections\AllHolidays();
-    }
-
-    public function testAssertEqualsAllHolidaysCollection()
-    {
-        $this->assertEquals(
-            $this->expectedCollectionDefault(),
-            $this->collection->getCollection()
-        );
+        $this->actualYear = (int) date('Y');
     }
 
     public function testAssertEqualLengthCollection()
@@ -199,34 +193,169 @@ class HolidaysTest
         );
     }
 
-    public function expectedCollectionDefault()
+    public function testAssertEqualsFormatterDate()
+    {
+        $this->assertEquals(
+            (new \Holidays\Types\NewYearsDay())->formatter(),
+            $this->collection
+                ->orderByTimestamp()
+                ->descending()
+                ->last()
+                ->formatter()
+        );
+    }
+
+    public function testAssertEqualsNextHolidayFormatter()
+    {
+        $this->assertEquals(
+            (new \Holidays\Types\NewYearsDay())->next(),
+            $this->collection
+                ->orderByTimestamp()
+                ->descending()
+                ->last()
+                ->next()
+        );
+    }
+
+    public function testAssertEqualsPreviousHolidayFormatter()
+    {
+        $this->assertEquals(
+            (new \Holidays\Types\NewYearsDay())->previous(),
+            $this->collection
+                ->orderByTimestamp()
+                ->descending()
+                ->last()
+                ->previous()
+        );
+    }
+
+    public function testExpectedExceptionDates()
+    {
+        $endDate = \DateTime::createFromFormat('d/m/Y', '01/01/2020')->setTime(0,0,0);
+        $startDate = \DateTime::createFromFormat('d/m/Y', '30/06/2020')->setTime(23,59,59);
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->collection
+            ->between($startDate, $endDate)
+            ->orderByTimestamp()
+            ->ascending()
+            ->getCollection();
+    }
+
+    public function testAssertEqualsCollectionFilterBetween()
+    {
+        $startDate = \DateTime::createFromFormat('d/m/Y', '01/01/2020')->setTime(0,0,0);
+        $endDate = \DateTime::createFromFormat('d/m/Y', '30/06/2020')->setTime(23,59,59);
+
+        $this->assertEquals(
+            $this->expectedCollectionFilterBetween($startDate, $endDate),
+            $this->collection
+                ->between($startDate, $endDate)
+                ->orderByTimestamp()
+                ->ascending()
+                ->getCollection()
+        );
+    }
+
+    public function testAssertEqualsCollectionFilterNotBetween()
+    {
+        $startDate = \DateTime::createFromFormat('d/m/Y', '01/06/2020')->setTime(0,0,0);
+        $endDate = \DateTime::createFromFormat('d/m/Y', '30/06/2020')->setTime(23,59,59);
+
+        $this->assertEquals(
+            $this->expectedCollectionFilterNotBetween($startDate, $endDate),
+            $this->collection
+                ->notBetween($startDate, $endDate)
+                ->orderByTimestamp()
+                ->ascending()
+                ->getCollection()
+        );
+    }
+
+    public function testAssertEqualsCollectionFilterLessThan()
+    {
+        $startDate = \DateTime::createFromFormat('d/m/Y', '01/06/2020')->setTime(0,0,0);
+
+        $this->assertEquals(
+            $this->expectedCollectionFilterLessThan($startDate),
+            $this->collection
+                ->lessThan($startDate)
+                ->orderByTimestamp()
+                ->ascending()
+                ->getCollection()
+        );
+    }
+
+    public function testAssertEqualsCollectionFilterLessThanEqual()
+    {
+        $startDate = \DateTime::createFromFormat('d/m/Y', '01/06/2020')->setTime(0,0,0);
+
+        $this->assertEquals(
+            $this->expectedCollectionFilterLessThan($startDate, true),
+            $this->collection
+                ->lessThanEqual($startDate)
+                ->orderByTimestamp()
+                ->ascending()
+                ->getCollection()
+        );
+    }
+
+    public function testAssertEqualsCollectionFilterGreaterThan()
+    {
+        $startDate = \DateTime::createFromFormat('d/m/Y', '01/11/2020')->setTime(0,0,0);
+
+        $this->assertEquals(
+            $this->expectedCollectionFilterGreaterThan($startDate),
+            $this->collection
+                ->greaterThan($startDate)
+                ->orderByTimestamp()
+                ->ascending()
+                ->getCollection()
+        );
+    }
+
+    public function testAssertEqualsCollectionFilterGreaterThanEqual()
+    {
+        $startDate = \DateTime::createFromFormat('d/m/Y', '01/11/2020')->setTime(0,0,0);
+
+        $this->assertEquals(
+            $this->expectedCollectionFilterGreaterThan($startDate, true),
+            $this->collection
+                ->greaterThanEqual($startDate)
+                ->orderByTimestamp()
+                ->ascending()
+                ->getCollection()
+        );
+    }
+
+    public function expectedCollectionDefault($year)
     {
         return [
-            new \Holidays\Types\AllSoulsDay(),
-            new \Holidays\Types\Carnival(),
-            new \Holidays\Types\ChildrenDay(),
-            new \Holidays\Types\ChristmasDay(),
-            new \Holidays\Types\CorpusChrist(),
-            new \Holidays\Types\DecemberSolstice(),
-            new \Holidays\Types\EasterSunday(),
-            new \Holidays\Types\FatherDay(),
-            new \Holidays\Types\GoodFriday(),
-            new \Holidays\Types\IndependenceBrazil(),
-            new \Holidays\Types\JuneSolstice(),
-            new \Holidays\Types\LaborDay(),
-            new \Holidays\Types\MarchEquinox(),
-            new \Holidays\Types\MotherDay(),
-            new \Holidays\Types\NewYearsDay(),
-            new \Holidays\Types\OurLadyOfAparecida(),
-            new \Holidays\Types\RepublicProclamationDay(),
-            new \Holidays\Types\SeptemberEquinox(),
-            new \Holidays\Types\TiradentesDay(),
+            new \Holidays\Types\AllSoulsDay($year),
+            new \Holidays\Types\Carnival($year),
+            new \Holidays\Types\ChildrenDay($year),
+            new \Holidays\Types\ChristmasDay($year),
+            new \Holidays\Types\CorpusChrist($year),
+            new \Holidays\Types\DecemberSolstice($year),
+            new \Holidays\Types\EasterSunday($year),
+            new \Holidays\Types\FatherDay($year),
+            new \Holidays\Types\GoodFriday($year),
+            new \Holidays\Types\IndependenceBrazil($year),
+            new \Holidays\Types\JuneSolstice($year),
+            new \Holidays\Types\LaborDay($year),
+            new \Holidays\Types\MarchEquinox($year),
+            new \Holidays\Types\MotherDay($year),
+            new \Holidays\Types\NewYearsDay($year),
+            new \Holidays\Types\OurLadyOfAparecida($year),
+            new \Holidays\Types\RepublicProclamationDay($year),
+            new \Holidays\Types\SeptemberEquinox($year),
+            new \Holidays\Types\TiradentesDay($year),
         ];
     }
 
     public function expectedCollectionOrderByNameAscending()
     {
-        $collection = $this->expectedCollectionDefault();
+        $collection = $this->expectedCollectionDefault($this->getActualYear());
 
         usort($collection, function(\Holidays\Contract\Holiday $a, \Holidays\Contract\Holiday $b) {
             return $a->getName() > $b->getName();
@@ -237,7 +366,7 @@ class HolidaysTest
 
     public function expectedCollectionOrderByNameDescending()
     {
-        $collection = $this->expectedCollectionDefault();
+        $collection = $this->expectedCollectionDefault($this->getActualYear());
 
         usort($collection, function(\Holidays\Contract\Holiday $a, \Holidays\Contract\Holiday $b) {
             return $a->getName() < $b->getName();
@@ -248,7 +377,7 @@ class HolidaysTest
 
     public function expectedCollectionOrderByTimestampAscending()
     {
-        $collection = $this->expectedCollectionDefault();
+        $collection = $this->expectedCollectionDefault($this->getActualYear());
 
         usort($collection, function(\Holidays\Contract\Holiday $a, \Holidays\Contract\Holiday $b) {
             return $a->getTimestamp() > $b->getTimestamp();
@@ -259,12 +388,61 @@ class HolidaysTest
 
     public function expectedCollectionOrderByTimestampDescending()
     {
-        $collection = $this->expectedCollectionDefault();
+        $collection = $this->expectedCollectionDefault($this->getActualYear());
 
         usort($collection, function(\Holidays\Contract\Holiday $a, \Holidays\Contract\Holiday $b) {
             return $a->getTimestamp() < $b->getTimestamp();
         });
 
         return $collection;
+    }
+
+    public function expectedCollectionFilterBetween(\DateTimeInterface $startDate, \DateTimeInterface $endDate)
+    {
+        $collection = array_filter($this->expectedCollectionOrderByTimestampAscending(), function (\Holidays\Contract\Holiday $holiday) use ($startDate, $endDate) {
+            return $holiday->getTimestamp() >= $startDate->getTimestamp() &&
+                $holiday->getTimestamp() <= $endDate->getTimestamp();
+        });
+
+        return array_values($collection);
+    }
+
+    public function expectedCollectionFilterNotBetween(\DateTimeInterface $startDate, \DateTimeInterface $endDate)
+    {
+        $collection = array_filter($this->expectedCollectionOrderByTimestampAscending(), function (\Holidays\Contract\Holiday $holiday) use ($startDate, $endDate) {
+            return $holiday->getTimestamp() < $startDate->getTimestamp() ||
+                $holiday->getTimestamp() > $endDate->getTimestamp();
+        });
+
+        return array_values($collection);
+    }
+
+    public function expectedCollectionFilterLessThan(\DateTimeInterface $startDate, $equal = false)
+    {
+        $collection = array_filter($this->expectedCollectionOrderByTimestampAscending(), function (\Holidays\Contract\Holiday $holiday) use ($startDate, $equal) {
+            if ($equal) {
+                return $holiday->getTimestamp() <= $startDate->getTimestamp();
+            }
+            return $holiday->getTimestamp() < $startDate->getTimestamp();
+        });
+
+        return array_values($collection);
+    }
+
+    public function expectedCollectionFilterGreaterThan(\DateTimeInterface $startDate, $equal = false)
+    {
+        $collection = array_filter($this->expectedCollectionOrderByTimestampAscending(), function (\Holidays\Contract\Holiday $holiday) use ($startDate, $equal) {
+            if ($equal) {
+                return $holiday->getTimestamp() >= $startDate->getTimestamp();
+            }
+            return $holiday->getTimestamp() > $startDate->getTimestamp();
+        });
+
+        return array_values($collection);
+    }
+
+    public function getActualYear()
+    {
+        return $this->actualYear;
     }
 }
